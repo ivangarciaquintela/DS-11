097aeaa68087cac9635f660a71df144048f19609
Ivan Garcia <ivan.garcia.quintela@udc.es>
2022-09-26 14:04:54 +0200

    E2

---
+++ DS-11-B1/src/e2/SocialDistance.java
@@ -0,0 +1,79 @@
+package e2;
+
+public class SocialDistance {
+
+    public static char [][] seatingPeople ( char [][] layout ) {
+        int height = layout.length;
+        int width = layout[0].length;
+        //int x  = (int)Math.random() * height;
+        //int y  = (int)Math.random() * width;
+        for (int i = 0; i < layout.length ; i++) {
+            for (int j = 0; j < layout[i].length; j++) {
+                if(libre(layout,i,j))
+                    layout[i][j] = '#';
+            }
+        }
+
+        /**comprobar si hay + de 4 sentados al rededor**/
+        return layout;
+    }
+
+    private static boolean libre(char [][] layout, int x, int y){
+        return (layout[x][y]=='A') && libreUP(layout, x, y) &&
+                libreDOWN(layout, x, y) &&
+                libreRIGHT(layout, x, y) &&
+                libreLEFT(layout, x, y) &&
+                libreLUP(layout, x, y) &&
+                libreRUP(layout, x, y) &&
+                libreRDOWN(layout, x, y) &&
+                libreLDOWN(layout, x, y);
+    }
+    private static boolean libreUP(char [][] layout, int x, int y) {
+        return (y>0)?layout[x][y-1]!='#':true;
+    }
+    private static boolean libreDOWN(char [][] layout, int x, int y) {
+        return (y<(layout[0].length-1))?layout[x][y+1]!='#':true;
+    }
+    private static boolean libreRIGHT(char [][] layout, int x, int y) {
+        return (x<layout.length-1)?layout[x+1][y]!='#':true;
+    }
+    private static boolean libreLEFT(char [][] layout, int x, int y) {
+        return (x>0)?layout[x-1][y]!='#':true;
+    }
+
+    private static boolean libreRUP(char [][] layout, int x, int y) {
+        return (y>0&&x<layout.length-1)?layout[x+1][y-1]!='#':true;
+    }
+    private static boolean libreRDOWN(char [][] layout, int x, int y) {
+        return (y<layout[0].length-1&&x<layout.length-1)?layout[x+1][y+1]!='#':true;
+    }
+    private static boolean libreLUP(char [][] layout, int x, int y) {
+        return (x>0&&y>0)?layout[x-1][y-1]!='#':true;
+    }
+    private static boolean libreLDOWN(char [][] layout, int x, int y) {
+        return (x>0&&y<layout[0].length-1)?layout[x-1][y+1]!='#':true;
+    }
+
+    public static void main(String[] args) {
+        /*char [][] l = new char[5][5];
+        for (int i = 0; i < l.length; i++) {
+            for (int j = 0; j < l[i].length; j++) {
+                int d = (int)(Math.random()*5);
+                if(d<1){
+                    l[i][j]='.';
+                }else{
+                    l[i][j]='A';
+                }
+            }
+        }*/
+        char [][] l = { {'A','.','A','A','.'},
+                        {'A','A','A','A','A'},
+                        {'A','.','A','.','A'},
+                        {'A','A','A','A','.'},
+                        {'A','.','A','A','.'}};
+        var b = l;
+        seatingPeople(l);
+        var a = "";
+    }
+
+}
